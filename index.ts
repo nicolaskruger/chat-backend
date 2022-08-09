e943bab9d1fd7454e7f6a6b7eb6c72a0fa80fd99
nkruger <nicolas.nkruger@gmail.com>
2022-08-09 17:22:46 -0300

    fix typo

--- index.ts
+++ index.ts
@@ -1,15 +1,20 @@
 import express from 'express'
 import cors from 'cors';
 import { router } from './src/routes';
+import { errorMiddleware } from './src/middlewares/erroMiddleware';
 const port = process.env.PORT || 5000;
 
 const app = express()
+
 
 app.use(express.json())
 
 app.use(cors())
 
+
 app.use(router)
+
+app.use(errorMiddleware)
 
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
